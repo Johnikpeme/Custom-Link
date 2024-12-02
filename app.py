788600a86aed82ac38a95dcac0282fcfca57462d
John Ikpeme <105491189+Johnikpeme@users.noreply.github.com>
2024-12-02 16:00:04 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
+import os
 from flask import Flask, redirect, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def redirect_to_store():
-    user_agent = request.headers.get('User-Agent')
+    user_agent = request.headers.get('User-Agent', 'Unknown')
+    print(f"User-Agent: {user_agent}")
 
     if "iPhone" in user_agent or "iPad" in user_agent:
         return redirect("https://apps.apple.com/si/app/nouns-hunt/id1667269607")
@@ -14,4 +16,5 @@ def redirect_to_store():
         return redirect("https://play.google.com/store/apps/details?id=com.DashStudios.NounsFlutter&hl=en")
 
 if __name__ == '__main__':
-    app.run()
+    port = int(os.environ.get("PORT", 8080))  # Use Render's provided PORT
+    app.run(host='0.0.0.0', port=port)
